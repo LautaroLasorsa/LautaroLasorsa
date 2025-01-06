f369fdd75942eca36f5e631d9bbf57b858346267
Lautaro_Lasorsa <lautarolasorsa@gmail.com>
2025-01-06 15:21:01 -0300

    README generated by Python

--- README.py
+++ README.py
@@ -8,10 +8,10 @@ text = """<div >
 alto    = 50
 pandas  = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-original-wordmark.svg" height={alto}/>"""
 numpy   = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/numpy/numpy-plain-wordmark.svg" height={alto}/>""" 
-python  = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-plain-wordmark.svg" height={alto}/>"""
+python  = f"""<img src="https://skillicons.dev/icons?i=py" height={alto}/>"""
 cpp     = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" height={alto}/>""" 
 sklearn = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg" height={alto}/>"""
-bash    = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bash/bash-original.svg" height={alto}/>"""
+bash    = f"""<img src="https://skillicons.dev/icons?i=bash" height={alto}/>"""
 R       = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/rstudio/rstudio-original.svg" height={alto}/>"""
 kotlin  = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/kotlin/kotlin-plain-wordmark.svg" height={alto}/>"""
 matplotlib = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/matplotlib/matplotlib-original.svg" height={alto}/>"""
@@ -19,6 +19,8 @@ jupyter = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icon
 tensorflow = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original-wordmark.svg" height={alto}/>"""
 IoT     = ""
 cuda    = ""
+latex   = f"""<img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/latex/latex-original.svg" height={alto}/>"""
+github_actions = f"""<img src="https://skillicons.dev/icons?i=githubactions" height={alto}/>"""
 
 sections = {}
 
@@ -26,9 +28,13 @@ sections["Competitive Programming"] = [
     ("Usefull competitive programming commands",
      "https://github.com/LautaroLasorsa/competitive-programming-suite",
      [cpp,python,bash]),
+     ("Problems I proposed to national and international contests",
+       "https://github.com/LautaroLasorsa/Mis-problemas--My-Problems",
+       [cpp, python, latex]     
+     ),
      ("Kotlin Notebook for ICPC teams", 
       "https://github.com/LautaroLasorsa/notebook-unlam-kotlin",
-      [kotlin,bash]),
+      [kotlin,bash,github_actions]),
       ("Python Notebook for ICPC teams",
        "https://github.com/LautaroLasorsa/notebook-unlam-python",
        [python,bash]),
@@ -43,7 +49,7 @@ sections["Competitive Programming"] = [
 sections["University Assigments"] = [
     ("Licenciature Tesis (10/10) <i>Propuesta de mejora del bienestar económico</i>",
      "https://github.com/LautaroLasorsa/Tesis-LCD-Lasorsa",
-     [python,numpy,pandas,matplotlib,jupyter, cpp,cuda]),
+     [python,numpy,pandas,matplotlib,jupyter, cpp,cuda, latex]),
     ("Instrumental Variable Quantile Regression module in R",
      "https://github.com/LautaroLasorsa/IVQR",
      [R])
